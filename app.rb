9738694ee1257762dbd6e3af0b527662cd9c010b
nichol alexander <nichol.alexander@gmail.com>
2015-08-14 14:32:53 -0400

    root path

--- app.rb
+++ app.rb
@@ -1,10 +1,11 @@
 require 'sinatra'
+require 'pry'
 
-get '/'
-  File.read('index.html')
+get '/' do
+  File.read('views/index.html')
 end
 
-get 'favorites' do
+get '/favorites' do
   response.header['Content-Type'] = 'application/json'
   File.read('data.json')
 end
@@ -13,6 +14,7 @@ get '/favorites' do
   file = JSON.parse(File.read('data.json'))
   unless params[:name] && params[:oid]
     return 'Invalid Request'
+  end
   movie = { name: params[:name], oid: params[:oid] }
   file << movie
   File.write('data.json',JSON.pretty_generate(file))
