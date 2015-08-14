cb632991e6d0d5ec6f13c0f5022ffd825fc7b25b
nichol alexander <nichol.alexander@gmail.com>
2015-08-14 15:42:24 -0400

    post route with errors in json fixed - incl. include json

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'sinatra'
 require 'sinatra/reloader'
+require 'json'
 require 'pry'
 
 class MyMovieApi < Sinatra::Base
@@ -7,24 +8,29 @@ class MyMovieApi < Sinatra::Base
     register Sinatra::Reloader
   end
 
-
+  # Set your root path
   get '/' do
     File.read('views/index.html')
   end
 
+  # list all the favorites (being stored in the data.json file)
   get '/favorites' do
     response.header['Content-Type'] = 'application/json'
     File.read('data.json')
   end
 
-  get '/favorites' do
+  # add a movie to your favorites - use the post method!
+  # our data.json is going to be a simple hash with the movie name
+  # returning a simple oid
+  # TODO handle errors when data.json is empty file
+  post '/favorites' do
     file = JSON.parse(File.read('data.json'))
     unless params[:name] && params[:oid]
       return 'Invalid Request'
     end
     movie = { name: params[:name], oid: params[:oid] }
-    file << movie
-    File.write('data.json',JSON.pretty_generate(file))
+    file[movie[:name]] = movie[:oid]
+    File.write('data.json', JSON.pretty_generate(file))
     movie.to_json
   end
 
